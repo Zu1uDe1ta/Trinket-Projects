33c7f3e880ecb23b857a7fd5483a688e533d01b3
Chris Chavez <chrischavezdataengineer@gmail.com>
2021-03-20 23:58:36 -0400

    EXPLORING TURTLE MODULE

--- Race_track.py
+++ Race_track.py
@@ -14,9 +14,17 @@ forward(20)
 write(5) 
 forward(20) 
 """
-
+speed(10)
+penup()
 goto(-140, 140)
 
 for step in range(6): 
-  write(step)
+  write(step, align='center')
+  right(90)
+  forward(10)
+  pendown()
+  forward(150)
+  penup()
+  backward(160)
+  left(90)
   forward(20)
