e6ebbbe8d5a584d456cff403afa0b4c7750af633
Chris Chavez <chrischavezdataengineer@gmail.com>
2021-03-21 00:09:29 -0400

    ADDED TURTLES TO THE GAME

--- Race_track.py
+++ Race_track.py
@@ -1,5 +1,5 @@
 from turtle import * 
-
+from random import randint
 """
 write(0) 
 forward(20) 
@@ -14,7 +14,7 @@ forward(20)
 write(5) 
 forward(20) 
 """
-speed(10)
+speed(0)
 penup()
 goto(-140, 140)
 
@@ -28,3 +28,23 @@ for step in range(6):
   backward(160)
   left(90)
   forward(20)
+  
+ada = Turtle()
+ada.color('red')
+ada.shape('turtle')
+
+ada.penup()
+ada.goto(-160, 100)
+ada.pendown()
+
+bob = Turtle()
+bob.color('blue')
+bob.shape('turtle')
+
+bob.penup()
+bob.goto(-160, 70)
+bob.pendown()
+
+for turn in range(100):
+  ada.forward(randint(1,5))
+  bob.forward(randint(1,5))
